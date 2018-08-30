eca93d25f07d521c15f71abd99b1c7d8c1af150e
JCJordan <jordan.craig2@hotmail.co.uk>
2018-08-30 16:17:19 +0100

    Tank Tracks now only drive when on the ground.

--- Source/BattleTank/Private/TankTrack.cpp
+++ Source/BattleTank/Private/TankTrack.cpp
@@ -5,7 +5,7 @@
 #include "Components/PrimitiveComponent.h"
 
 UTankTrack::UTankTrack() {
-	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bCanEverTick = false;
 }
 
 void UTankTrack::BeginPlay() {
@@ -14,28 +14,34 @@ void UTankTrack::BeginPlay() {
 
 void UTankTrack::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit) {
 
+	DriveTrack();
+	ApplyCounterSidewaysForce();
+	CurrentThrottle = 0;
 }
 
-void UTankTrack::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) {
-
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
+void UTankTrack::ApplyCounterSidewaysForce()
+{
 	float SidewaysSpeed = FVector::DotProduct(GetRightVector(), GetComponentVelocity());
+	float DeltaTime = GetWorld()->GetDeltaSeconds();
 	FVector CorrectionAcceleration = -1 * SidewaysSpeed / DeltaTime * GetRightVector();
 	UStaticMeshComponent* TankRoot = dynamic_cast<UStaticMeshComponent*>(GetOwner()->GetRootComponent());
 	FVector CorrectionForce = (TankRoot->GetMass() * CorrectionAcceleration) / 2; // Divide by 2 as 2 tracks
-	//TankRoot->AddForce(CorrectionForce);
+	TankRoot->AddForce(CorrectionForce);
 }
 
 void UTankTrack::SetThrottle(float Throttle) {
 
-	FVector ForceApplied = GetForwardVector() * Throttle * MaxDrivingForce;
+	CurrentThrottle = FMath::Clamp<float>(CurrentThrottle + Throttle, -1, 1);
+}
+
+void UTankTrack::DriveTrack()
+{
+	FVector ForceApplied = GetForwardVector() * CurrentThrottle * MaxDrivingForce;
 	FVector ForceLocation = GetComponentLocation();
 	UPrimitiveComponent* TankRoot = dynamic_cast<UPrimitiveComponent*>(GetOwner()->GetRootComponent());
-	
+
 	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
 
 	return;
-
 }
 
--- Source/BattleTank/Public/TankTrack.h
+++ Source/BattleTank/Public/TankTrack.h
@@ -16,12 +16,17 @@ public:
 
 	UTankTrack();
 	virtual void BeginPlay() override;
-	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) override;
+
+	// To stop tank sideways slipping.
+	void ApplyCounterSidewaysForce();
 
 	// Sets throttle between -1 and 1.
 	UFUNCTION(BlueprintCallable)
 	void SetThrottle(float Throttle);
+
+	void DriveTrack();
 	
+	// TODO Make better speed setup e.g. Acceleration and Mass used to calculate Force.
 	// Set max driving force that throttle can cause.
 	UPROPERTY(EditDefaultsOnly)
 	float MaxDrivingForce = 40000000; // 40 ton tank (kg) * 1000g acceleration (1000000cm/s)
@@ -30,5 +35,7 @@ private:
 
 	UFUNCTION()
 	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
+	float CurrentThrottle = 0;
 	
 };
